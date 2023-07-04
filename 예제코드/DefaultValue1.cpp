3b87a04dd8df85dd308d5fff6dc6727a06c9c740
xogns <xogns40430@naver.com>
2023-07-04 20:05:53 +0900

    함수의 매개변수 디폴트 값

---
+++ 예제코드/DefaultValue1.cpp
@@ -0,0 +1,16 @@
+// DefaultValue1.cpp -- 함수의 매개변수 디폴트 값
+#include <iostream>
+
+int Adder(int num1 = 1, int num2 = 2)
+{
+  return num1 + num2;
+}
+
+int main(void)
+{
+  std:: cout<< Adder()<< std:: endl;
+  std:: cout<< Adder(5)<< std:: endl;
+  std:: cout<< Adder(3, 5)<< std:: endl;
+
+  return 0;
+}
