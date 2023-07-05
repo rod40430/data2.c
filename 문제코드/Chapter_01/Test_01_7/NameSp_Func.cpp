3081695d3e9d5b7f481ac41f205ed4a39e626ace
xogns <xogns40430@naver.com>
2023-07-05 22:49:33 +0900

    문제 01 - 7 [C++ 기반의 데이터 입출력]

---
+++ 문제코드/Chapter_01/Test_01_7/NameSp_Func.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "NameSp.hpp"
+
+void BestComImp1:: SimpleFunc(void)
+{
+  std:: cout<< "BestCom이 정의한 함수"<< std:: endl;
+}
+
+void ProgComImp1:: SimpleFunc(void)
+{
+  std:: cout<< "ProgCom이 정의한 함수"<< std:: endl;
+}
