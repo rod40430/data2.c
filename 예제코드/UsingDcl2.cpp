6fdefc8ab87d1315336716a658c30c2b8767fa94
xogns <xogns40430@naver.com>
2023-07-05 23:01:11 +0900

    이름공간 std에 선언된 모든것에 대해 이름공간 지정의 생략

---
+++ 예제코드/UsingDcl2.cpp
@@ -0,0 +1,15 @@
+// UsingDcl2.cpp -- 이름공간 std에 선언된 모든것에 대해 이름공간 지정의 생략
+#include <iostream>
+
+using std:: cin;
+using std:: cout;
+using std:: endl;
+
+int main(void)
+{
+  int num = 20;
+  cout<< "Hello World!"<< endl;
+  cout<< "Hello "<< "World!"<< endl;
+  cout<< num<< ' '<< 'A';
+  cout<< ' '<< 3.14<< endl;
+}
