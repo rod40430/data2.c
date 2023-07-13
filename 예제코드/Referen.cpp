34abc265e45f661bcbd5b18601e05e1c749a034f
xogns <xogns40430@naver.com>
2023-07-13 13:56:09 +0900

    참조자는 자신이 참조하는 변수를 대신할 수 있는 또 하나의 이름

---
+++ 예제코드/Referen.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  int num1 = 1020;
+  int &num2 = num1;
+
+  num2 = 3047;
+  cout<<"VAL : "<<num1<<endl;
+  cout<<"REF : "<<num2<<endl;
+
+  cout<<"VAL : "<<&num1<<endl;
+  cout<<"REF : "<<&num2<<endl;
+
+  return 0;
+}
