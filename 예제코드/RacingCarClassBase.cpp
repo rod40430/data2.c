713f000646d07e3d19ea52faa1736fbe085bfbea
xogns <xogns40430@naver.com>
2023-07-18 14:20:51 +0900

    구조체대신 클래스를 사용한다.
    구조체와 클래스의 차이점은 접근제어에 있다.

---
+++ 예제코드/RacingCarClassBase.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstring>
+
+namespace CAR_CONST
+{
+  enum
+  {
+    ID_LEN = 20,
+    MAX_SPD = 200,
+    FUEL_STEP = 2,
+    ACC_STEP = 10,
+    BRK_STEP = 10
+  };
+}
+
+using namespace CAR_CONST;
+using namespace std;
+
+typedef class Car
+{
+  private:
+    int fuelGauge;
+    int curSpeed;
+    char gamerID[ID_LEN];
+
+  public:
+    void InitMembers(char *ID, int fuel);
+    void ShowCarState();  // 상태정보 출력
+    void Accel();  // 액셀, 속도증가
+    void Break();  // 브레이크, 속도감소
+} car;
+
+int main(void)
+{
+  car run99;
+  char id[] = "run99";
+  
+  run99.InitMembers(id, 100);
+  run99.Accel();
+  run99.Accel();
+  run99.Accel();
+  run99.ShowCarState();
+  run99.Break();
+  run99.ShowCarState();
+
+  return 0;
+}
+
+void car::InitMembers(char *ID, int fuel)
+{
+  strcpy(gamerID, ID);
+  fuelGauge = fuel;
+  curSpeed = 0;
+}
+
+void car::ShowCarState()
+{
+  cout<<"소유자ID : "<<gamerID<<endl;
+  cout<<"연료량 : "<<fuelGauge<<"%"<<endl;
+  cout<<"현재속도 : "<<curSpeed<<"Km/s"<<endl<<endl;
+}
+
+void car::Accel()
+{
+  if (fuelGauge <= 0)
+    return;
+    
+  else
+    fuelGauge -= FUEL_STEP;
+
+  if((curSpeed + ACC_STEP) >= MAX_SPD)
+  {
+    curSpeed = MAX_SPD;
+    return;
+  }
+
+  curSpeed += ACC_STEP;
+}
+
+void car::Break()
+{
+  if (curSpeed < BRK_STEP)
+  {
+    curSpeed = 0;
+    return;
+  }
+
+  curSpeed -= BRK_STEP;
+}
