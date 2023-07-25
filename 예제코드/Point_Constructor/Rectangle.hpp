e41c93d9d590653063cda71ba5f6f4168fa86596
xogns <xogns40430@naver.com>
2023-07-25 13:37:53 +0900

    Rectangle.hpp

---
+++ 예제코드/Point_Constructor/Rectangle.hpp
@@ -0,0 +1,17 @@
+#ifndef __RECTANGLE_H_
+#define __RECTANGLE_H_
+
+#include "Point.hpp"
+
+class Rectangle
+{
+private:
+  Point upLeft;
+  Point lowRight;
+
+public:
+  Rectangle(const int &x1, const int &y1, const int &x2, const int &y2);
+  void ShowRecInfo() const;
+};
+
+#endif
