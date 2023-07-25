3ae477b8683445862080f923cd687fa4e948dc66
xogns <xogns40430@naver.com>
2023-07-25 14:22:57 +0900

    멤버 이니셜라이저는 참조자를 멤버변수로 선언하는것도 가능하다.
    가능하지만 흔한 것은 아니다.

---
+++ 예제코드/ReferenceMember.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class AAA
+{
+public:
+  AAA()
+  {
+    cout<<"empty object"<<endl;
+  }
+
+  void ShowYourName()
+  {
+    cout<<"I'm class AAA"<<endl;
+  }
+};
+
+class BBB
+{
+private:
+  AAA &ref;
+  const int &num;
+
+public:
+  BBB(AAA &r, const int &n)
+    :ref(r), num(n)
+  {
+    // empty constructor body
+  }
+
+  void ShowYourName()
+  {
+    ref.ShowYourName();
+    cout<<"and"<<endl;
+    cout<<"I ref num "<<num<<endl;
+  }
+};
+
+int main(void)
+{
+  AAA obj1;
+  BBB obj2(obj1, 20);
+
+  obj2.ShowYourName();
+
+  return 0;
+}
