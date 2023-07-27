78012b459cb17db9769cc4860908a6e87181bb03
xogns <xogns40430@naver.com>
2023-07-27 12:51:08 +0900

    객체 포인터배열 선언

---
+++ 예제코드/ObjPtrArr.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Person
+{
+private:
+  char * name;
+  int age;
+
+public:
+  Person(char * myname, int myage)
+  {
+    int len = strlen(myname) + 1;
+    name = new char[len];
+    strcpy(name, myname);
+    age = myage;
+  }
+  Person()
+  {
+    name = NULL;
+    age = 0;
+    cout<<"called Person()"<<endl;
+  }
+  void SetPersonInfo(char * myname, int myage)
+  {
+    name = myname;
+    age = myage;
+  }
+  void ShowPersonInfo() const
+  {
+    cout<<"이름 : "<<name<<", ";
+    cout<<"나이 : "<<age<<endl;
+  }
+  ~Person()
+  {
+    delete []name;
+    cout<<"called destructor!"<<endl;
+  }
+};
+
+int main(void)
+{
+  Person * parr[3];
+  char namestr[100];
+  int age;
+
+  for(int i = 0; i < 3; i++)
+    {
+      cout<<"이름 : ";
+      cin>>namestr;
+      cout<<"나이 : ";
+      cin>>age;
+      parr[i] = new Person(namestr, age);
+    }
+  for(int i = 0; i < 3; i++)
+    {
+      parr[i]->ShowPersonInfo();
+      delete parr[i];
+    }
+
+  return 0;
+}
