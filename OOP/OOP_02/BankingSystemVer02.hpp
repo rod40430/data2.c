04ceaaa1fc6a081bc8763495943d3a2db579a0d0
xogns <xogns40430@naver.com>
2023-07-30 23:22:40 +0900

    OOP 단게별 프로젝트 02단계

--- OOP/OOP_02/BankingSystemVer02.hpp
+++ OOP/OOP_02/BankingSystemVer02.hpp
@@ -1,21 +1,11 @@
+// OOP 단게별 프로젝트 02단계
 #ifndef __BankingSystemVer_H_
 #define __BankingSystemVer_H_
 
-// OOP 단게별 프로젝트 02단계
+const int MAX_USER = 100;
+const int MAX_STR = 20;
 
-int const USER_COUNT = 20;
-
-namespace Menu
-{
-  enum Menu
-  {
-    CREATE_ACCOUNT = 1,
-    INCASH, OUTCASH, 
-    SHOW_ACCOUNT,
-    EIXT
-  };
-}
-
+enum {CREATE = 1, INCASH, OUTCASH, SHOWINFO, EXIT};
 
 class User
 {
@@ -23,16 +13,20 @@ private:
   int userid;
   int ucash;
   char * uname;
-  int count;
 
 public:
   User(int id, int cash, char * name);
-  void CreateAccount(void);  // 계좌생성
-  void InCash(void);  // 입금
-  void OutCash(void);  // 출금
-  void ShowAccount(void) const;  //잔액 조회
+  int GetID(void);
+  void InCash(int money);
+  int OutCash(int money);
+  void ShowAccount(void);
   ~User();
 };
-int ShowMenu(void);  //메뉴출력
+
+void ShowMenu(void);  //메뉴출력
+void CreateAccount(User **user, int count);  // 계좌개설
+void InCash(User *user, int count);  // 입금
+void OutCash(User *user, int count);  // 출금
+void ShowAccount(User *user, int count);  //잔액 조회
 
 #endif
