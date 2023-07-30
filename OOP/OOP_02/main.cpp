616358f1d402308cd902e792a4f4e57ac900a4b5
xogns <xogns40430@naver.com>
2023-07-30 23:24:42 +0900

    메인 함수

---
+++ OOP/OOP_02/main.cpp
@@ -0,0 +1,47 @@
+#include "BankingSystemVer02.hpp"
+#include <iostream>
+
+using namespace std;
+
+
+int main(void) {
+  User * user[MAX_USER];
+  int choice;
+  int count = 0;
+
+  while (1) {
+    ShowMenu();
+    cin >> choice;
+    cout << endl;
+
+    if (choice == CREATE) {
+      CreateAccount(user, count);
+      count++;
+    }
+
+    else if (choice == INCASH) {
+      InCash(*user, count);
+    }
+
+    else if (choice == OUTCASH) {
+      OutCash(*user, count);
+    }
+
+    else if (choice == SHOWINFO) {
+      ShowAccount(*user, count);
+    }
+
+    else if (choice == EXIT) {
+      cout << "프로그램을 종료합니다."<<endl;
+      for (int i = 0; i < count; i++)
+        delete user[i];
+      exit(1);
+    }
+
+    else {
+      cout << "1~5번 중에 골라주세요.";
+    }
+  }
+
+  return 0;
+}
