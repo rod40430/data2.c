6ff1c8659e8a99fceb2e300948eaffe3b83bedc2
xogns <xogns40430@naver.com>
2023-07-31 23:36:36 +0900

    임시객체가 소멸해도 임시객체가 생성된 자리에는 임시객체 참조값이 반환된다.

---
+++ 예제코드/IKnowTempObj.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Temporary
+{
+private:
+  int num;
+public:
+  Temporary(int n) : num(n)
+  {
+    cout<<"create obj : "<<num<<endl;
+  }
+  ~Temporary()
+  {
+    cout<<"destroy obj : "<<num<<endl;
+  }
+  void ShowTempInfo()
+  {
+    cout<<"My num is "<<num<<endl;
+  }
+};
+
+int main(void)
+{
+  Temporary(100);
+  cout<<"********** after make!"<<endl<<endl;
+
+  Temporary(200);
+  cout<<"********** after make!"<<endl<<endl;
+
+  const Temporary &ref = Temporary(300);
+  cout<<"********** and of main!"<<endl<<endl;
+
+  return 0;
+}
