6617e0d91bbdfcb03bc5b1a205936cd2c98462f2
xogns <xogns40430@naver.com>
2023-08-01 22:28:01 +0900

    const로 클래스를 생성하면 const멤버함수 이외에는 호출이 불가능하다.

---
+++ 예제코드/ConstObject.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class SoSimple
+{
+private:
+  int num;
+public:
+  SoSimple(int n) : num(n) {}
+  SoSimple& AddNum(int n)
+  {
+    num += n;
+    return *this;
+  }
+  void ShowData() const
+  {
+    cout<<"num : "<<num<<endl;
+  }
+};
+
+int main(void)
+{
+  const SoSimple obj(7);
+  // obj.AddNum(20);
+  obj.ShowData();
+
+  return 0;
+}
