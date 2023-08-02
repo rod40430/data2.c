b66d62076ef961db069f27c41dbc9d2b52b948dc
xogns <xogns40430@naver.com>
2023-08-02 10:02:36 +0900

    static의 개념 복습
    전역변수에 선언된 static의 의미
    -> 선언된 파일 내에서만 참조를 허용하겠다는 의미
    
    함수 내에 선언된 static의 의미
    -> 한번만 초기화되고, 지역변수와 달리 함수를 빠져나가도
    소멸되지 않는다.

---
+++ 예제코드/CComStatic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void Counter()
+{
+  static int cnt;
+  cnt++;
+  cout<<"Current cnt : "<<cnt<<endl;
+}
+
+int main(void)
+{
+  for (int i = 0; i < 10; i++)
+    Counter();
+
+  return 0;
+}
