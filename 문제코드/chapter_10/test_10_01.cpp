c8590930564f899044e53f8627502a59eb970a17
xogns <xogns40430@naver.com>
2023-08-21 19:38:03 +0900

    문제_10_01 [두 가지 방법의 연산자 오버로딩]

--- 문제코드/chapter_10/test_10_01.cpp
+++ 문제코드/chapter_10/test_10_01.cpp
@@ -13,9 +13,22 @@ public:
   {
     cout<<'['<<xpos<<", "<<ypos<<']'<<endl;
   }
+  Point& operator+=(const Point &ref)
+  {
+    this->xpos += ref.xpos;
+    this->ypos += ref.ypos;
+    return *this;
+  }
+
+  Point& operator-=(const Point &ref)
+  {
+    this->xpos -= ref.xpos;
+    this->ypos -= ref.ypos;
+    return *this;
+  }
+
   friend Point operator+(const Point &pos1, const Point &pos2);
-  friend Point operator+=(const Point &pos1, const Point &pos2);
-  friend Point operator-=(const Point &pos1, const Point &pos2);
+  friend Point operator-(const Point &pos1, const Point &pos2);
   friend bool operator==(const Point &pos1, const Point &pos2);
   friend bool operator!=(const Point &pos1, const Point &pos2);
 };
@@ -26,25 +39,15 @@ Point operator+(const Point &pos1, const Point &pos2)
   return pos;
 }
 
-Point operator+=(const Point &pos1, const Point &pos2)
+Point operator-(const Point &pos1, const Point &pos2)
 {
-  Point pos(pos1);
-  pos.xpos += pos2.xpos;
-  pos.ypos += pos2.ypos;
-  return pos;
-}
-
-Point operator-=(const Point &pos1, const Point &pos2)
-{
-  Point pos(pos1);
-  pos.xpos -= pos2.xpos;
-  pos.ypos -= pos2.ypos;
+  Point pos(pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos);
   return pos;
 }
 
 bool operator==(const Point &pos1, const Point &pos2)
 {
-  if (pos1 == pos2)
+  if (pos1.xpos == pos2.xpos && pos1.ypos == pos2.ypos)
     return true;
   else
     return false;
@@ -52,29 +55,29 @@ bool operator==(const Point &pos1, const Point &pos2)
 
 bool operator!=(const Point &pos1, const Point &pos2)
 {
-  if (pos1 != pos2)
-    return false;
-  else
-    return true;
+  return !(pos1 == pos2);  // 오버로딩 된 == 연산자 호출
 }
 
 int main(void)
 {
   Point pos1(3, 4);
-  Point pos2(10, 20);
-  Point pos3 = pos1 + pos2;
-  Point pos4 = pos1 += pos2;
-  Point pos5 = pos1 -= pos2;
-  Point pos6 = pos1 == pos2;
-  Point pos7 = pos1 != pos2;
+  Point pos2(5, 7);
+  Point pos3(5, 7);
 
-  pos1.ShowPosition();
-  pos2.ShowPosition();
-  pos3.ShowPosition();
-  pos4.ShowPosition();
-  pos5.ShowPosition();
-  pos6.ShowPosition();
-  pos7.ShowPosition();
+  (pos1 - pos2).ShowPosition();  // 객체를 반환하므로 함수호출 가능
+  (pos1 += pos2).ShowPosition();  // 반환형이 참조형이므로 함수호출 가능
+
+  if (pos2 == pos3)
+    cout<<"equal"<<endl;
+  else
+    cout<<"not equal"<<endl;
+
+  (pos2 -= pos3).ShowPosition(); // 반환형이 참조형이므로 함수호출 가능
+
+  if (pos2 != pos3)
+    cout<<"not equal"<<endl;
+  else
+    cout<<"equal"<<endl;
 
   return 0;
 }
