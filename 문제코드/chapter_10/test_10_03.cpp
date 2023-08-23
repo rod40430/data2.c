c3e085d109806aaea029f71901312f1362ccdbed
xogns <xogns40430@naver.com>
2023-08-23 22:48:58 +0900

    10_03 문제 [입력을 위한 >> 연산자의 오버로딩]

---
+++ 문제코드/chapter_10/test_10_03.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class Point
+{
+private:
+  int xpos, ypos;
+public:
+  Point(int x = 0, int y = 0) : xpos(x), ypos(y)
+  {}
+  void ShowPosition() const
+  {
+    cout<<'['<<xpos<<", "<<ypos<<']'<<endl;
+  }
+  friend istream& operator>>(istream& is, Point &pos);
+  friend ostream& operator<<(ostream& os, const Point &pos);
+};
+
+ostream& operator<<(ostream& os, const Point &pos)
+{
+  os<<'['<<pos.xpos<<", "<<pos.ypos<<']'<<endl;
+  return os;
+}
+
+istream& operator>>(istream& is, Point &pos)
+{
+  is>>pos.xpos>>pos.ypos;
+  return is;
+}
+
+int main(void)
+{
+  Point pos1;
+  cout<<"x, y 좌표 순으로 입력 : ";
+  cin>>pos1;
+  cout<<pos1;
+
+  Point pos2;
+  cout<<"x, y 좌표 순으로 입력 : ";
+  cin>>pos2;
+  cout<<pos2;
+
+  return 0;
+}
