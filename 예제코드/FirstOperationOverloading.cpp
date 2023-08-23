ea1ae51341e8e1734c934d0ee940e83e852fc781
xogns <xogns40430@naver.com>
2023-08-23 23:31:01 +0900

    디폴트 대입 연산자

--- 예제코드/FirstOperationOverloading.cpp
+++ 예제코드/FirstOperationOverloading.cpp
@@ -2,33 +2,54 @@
 
 using namespace std;
 
-class Point
+class First
 {
 private:
-  int xpos, ypos;
+  int num1, num2;
 public:
-  Point(int x = 0, int y = 0) : xpos(x), ypos(y)
+  First(int n1 = 0, int n2 = 0) : num1(n1), num2(n2)
   {}
-  void ShowPosition() const
+  void ShowData() {cout<<num1<<", "<<num2<<endl;}
+};
+
+class Second
+{
+private:
+  int num3, num4;
+public:
+  Second(int n3 = 0, int n4 = 0) : num3(n3), num4(n4)
+  {}
+  void ShowData() {cout<<num3<<", "<<num4<<endl;}
+  Second& operator=(const Second& ref)
   {
-    cout<<'['<<xpos<<", "<<ypos<<']'<<endl;
-  }
-  Point operator+(const Point &ref)  // operator+라는 이름의 함수
-  {
-    Point pos(xpos + ref.xpos, ypos + ref.ypos);
-    return pos;
+    cout<<"Second& operator=()"<<endl;
+
+    num3 = ref.num3;
+    num4 = ref.num4;
+    return *this;
   }
 };
 
 int main(void)
 {
-  Point pos1(3, 4);
-  Point pos2(10, 20);
-  Point pos3 = pos1.operator+(pos2);
+  First fsrc(111, 222);
+  First fcpy;
+  Second ssrc(333, 444);
+  Second scpy;
+  fcpy = fsrc;
+  scpy = ssrc;
+  fcpy.ShowData();
+  scpy.ShowData();
 
-  pos1.ShowPosition();
-  pos2.ShowPosition();
-  pos3.ShowPosition();
-  
+  First fob1, fob2;
+  Second sob1, sob2;
+  fob1 = fob2 = fsrc;
+  sob1 = sob2 = ssrc;
+
+  fob1.ShowData();
+  fob2.ShowData();
+  sob1.ShowData();
+  sob2.ShowData();
+
   return 0;
 }
