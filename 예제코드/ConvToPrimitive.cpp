775f2d472d787e66b61e01e34b7171b4b7351381
xogns <xogns40430@naver.com>
2023-08-28 16:33:57 +0900

    형 변환 연산자의 오버로딩

---
+++ 예제코드/ConvToPrimitive.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Number
+{
+private:
+  int num;
+public:
+  Number(int n = 0) : num(n)
+  {
+    cout<<"Number(int n = 0)"<<endl;
+  }
+  Number& operator= (const Number& ref)
+  {
+    cout<<"operator=()"<<endl;
+    this->num = ref.num;
+    return *this;
+  }
+  operator int () 
+  {
+    return num;
+  }
+  void ShowNumber() {cout<<num<<endl;}
+};
+
+int main(void)
+{
+  Number num;
+  num = 30;
+  Number num2 = num + 20;
+  num2.ShowNumber();
+
+  return 0;
+}
