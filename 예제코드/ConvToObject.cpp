91ba338c4c865233a5b68ecf57a66049ed3a1cfb
xogns <xogns40430@naver.com>
2023-08-28 15:45:17 +0900

    임시객체로의 자동 형 변환과 형 변환 연산자 (Conversion Operator)
    위의 num = 30 코드를 풀어서 해석하자면
    num = Number(30); // int 자료형을 Number자료형으로 형변환 및 임시객체 생성
    num.operator=(Number(30)); // 임시객체를 대상으로 하는 대입 연산자의 호출

---
+++ 예제코드/ConvToObject.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Number
+{
+private:
+  int num;
+public:
+  Number(int n = 0) : num(n)
+  {
+    cout<<"Number(int n = 0)"<<endl;
+  }
+  Number& operator= (const Number& ref)
+  {
+    cout<<"operator=()"<<endl;
+    this->num = ref.num;
+    return *this;
+  }
+  void ShowNumber() {cout<<num<<endl;}
+};
+
+int main(void)
+{
+  Number num;
+  num = 30;
+  num.ShowNumber();
+
+  return 0;
+}
