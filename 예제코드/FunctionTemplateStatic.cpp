dab43fcb3ac3d7eb7d83b4512c82acfe8f5cd8d6
xogns <xogns40430@naver.com>
2023-09-04 09:16:54 +0900

    함수 템플릿과 static 지역변수

---
+++ 예제코드/FunctionTemplateStatic.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void ShowStaticValue(void)
+{
+  static T num = 0;
+  num += 1;
+  cout<<num<<", ";
+}
+
+int main(void)
+{
+  ShowStaticValue<int>();
+  ShowStaticValue<int>();
+  ShowStaticValue<int>();
+  cout<<endl;
+  
+  ShowStaticValue<long>();
+  ShowStaticValue<long>();
+  ShowStaticValue<long>();
+  cout<<endl;
+
+  ShowStaticValue<double>();
+  ShowStaticValue<double>();
+  ShowStaticValue<double>();
+  cout<<endl;
+
+  return 0;
+}
